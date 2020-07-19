a2f7aeec3830848e6f65cd20b4779af842f94477
Shobhit Kulshreshtha <kulshreshtha.shobhit@gmail.com>
2020-07-19 10:44:21 +0530

    Added Jupyter notebook

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # SMS-Message-Spam-Detector
 A simple Flask App to classify messages as Ham/Spam using Python and Naive Bayes algorithm from Scikit Learn.
-The dataset used for training the model was taken from [Kaggle](https://www.kaggle.com/ucilml/sms-spam-collection-dataset). I removed few empty columns from the dataset and used it for training.
+The dataset used for training the model was taken from [Kaggle](https://www.kaggle.com/uciml/sms-spam-collection-dataset). I removed few empty columns from the dataset and used it for training.
--- app.py
+++ app.py
@@ -1,12 +1,62 @@
 from flask import Flask,render_template,url_for,request
-import pandas as pd 
-import pickle
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import MultinomialNB
+import pandas as pd
+import string
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
 from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
 
 
 app = Flask(__name__)
+
+# Adding in code from the Jupyter for training the model
+sms = pd.read_csv("spam.csv", encoding="latin-1")
+sms.columns = ['label', 'message']
+# Features and Labels
+sms['label_num'] = sms['label'].map({'ham': 0, 'spam': 1})
+	
+def text_process(message):
+	"""
+	Takes in a string of text and performs the following steps:
+	1. Remove punctuation
+	2. Tokenize
+	3. Remove stopwords
+	4. Stems words to their root forms
+	Return type: string
+	Returns: String of cleaned & stemmed message
+	"""
+	STOPWORDS = stopwords.words('english') + ['u', 'ü', 'ur', '4', '2', 'im', 'dont', 'doin', 'ure']
+	# Check characters to see if they are in punctuation
+	nopunc = [char for char in message if char not in string.punctuation]
+
+	# Join the characters again to form the string
+	nopunc = ''.join(nopunc)
+	
+	# Instantiating a PorterStemmer object
+	porter = PorterStemmer()
+	token_words = word_tokenize(nopunc)
+	stem_message=[]
+	for word in token_words:
+		stem_message.append(porter.stem(word))
+		stem_message.append(" ")
+	return ''.join(stem_message)
+
+sms['clean_message'] = sms['message'].apply(text_process)
+
+X = sms.clean_message
+y = sms.label_num
+
+pipe = Pipeline([('bow', CountVectorizer()), 
+				 ('tfid', TfidfTransformer()),  
+				 ('model', LogisticRegression(solver='liblinear'))])
+
+pipe.fit(X, y)
+	
 
 @app.route('/')
 def home():
@@ -14,28 +64,11 @@ def home():
 
 @app.route('/Predict',methods=['POST'])
 def Predict():
-	df= pd.read_csv("spam.csv", encoding="latin-1")
-	# Features and Labels
-	df['label'] = df['class'].map({'ham': 0, 'spam': 1})
-	X = df['message']
-	y = df['label']
 	
-	# Extract Feature With CountVectorizer
-	cv = CountVectorizer()
-	X = cv.fit_transform(X) # Learn Vocab and create document-term matrix
-	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=53) # Train Test Split
-
-	clf = MultinomialNB() # Initializing NaiveBayes Model
-	clf.fit(X_train,y_train) # Fit the model
-	# clf.score(X_test,y_test)
-
 	if request.method == 'POST':
 		message = request.form['message']
 		data = [message]
-		vect = cv.transform(data).toarray() # Create document-term matrix
-		my_prediction = clf.predict(vect)
+		my_prediction = pipe.predict(data)
 	return render_template('result.html',prediction = my_prediction)
 
-
-if __name__ == '__main__':
-	app.run(debug=True)
+app.run(debug=True)
